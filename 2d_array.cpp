12cd58b9d23bdb3bb69ec01e1b21f505ee8549d4
Minhajul Islam <mmminhaj221@gmail.com>
2024-10-17 21:29:17 +0600

    Create 2d_array.cpp

---
+++ 2d_array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std ;
+int main() {
+    int row ,avg , col ;
+    cin >> row >> col ;
+    int arr[row][col];
+    for(int i = 0 ; i<row ; i++){
+        for (int j = 0 ; j<col ; j++){
+            cin >> arr[i][j];
+        }
+    }
+    int highest = arr[0][0] , lowest = arr[0][0] ;
+    for(int i = 0 ; i<row ; i++){
+        for (int j = 0 ; j<col ; j++){
+            if( arr[i][j] > highest ) highest = arr[i][j] ;
+            if( arr[i][j] < lowest ) lowest = arr[i][j] ;
+        }
+        cout << endl ;
+    }
+    
+    cout << "highest : " << highest << endl ;
+    cout << "lowest : " << lowest << endl ;
+
+    return 0;
+}
