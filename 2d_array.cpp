8a39b11204b4887e6e5e0cef6511f75a8ac1061a
Minhajul Islam <mmminhaj221@gmail.com>
2024-10-17 21:34:54 +0600

    Update 2d_array.cpp

--- 2d_array.cpp
+++ 2d_array.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std ;
 int main() {
-    int row ,avg , col ;
+    int row ,avg , col , sum = 0 ;
     cin >> row >> col ;
     int arr[row][col];
     for(int i = 0 ; i<row ; i++){
@@ -14,12 +14,13 @@ int main() {
         for (int j = 0 ; j<col ; j++){
             if( arr[i][j] > highest ) highest = arr[i][j] ;
             if( arr[i][j] < lowest ) lowest = arr[i][j] ;
+            sum += arr[i][j];
         }
         cout << endl ;
     }
     
     cout << "highest : " << highest << endl ;
     cout << "lowest : " << lowest << endl ;
-
+    cout << "average : " << sum/(row*col);
     return 0;
 }
